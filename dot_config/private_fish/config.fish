2c4854f7458eb6750f2d3b1cc093e0bb90075bd1
Christian Lemer <christian@lemer.be>
2023-02-18 13:07:51 +0100

    fish: add starship and adjust some aliases and abbrevs.

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -4,37 +4,50 @@ if status is-interactive
     # 
     # Update Path
     #
-    
-    # Add local binaries
     fish_add_path .local/bin
-    # Add cargo binaries
     fish_add_path .cargo/bin
 
+    #
+    # pbcopy on ubuntu
+    #
+    alias pbcopy="xclip -selection clipboard"
+    alias pbpaste="xclip -selection clipboard -o"
 
     #
-    # Abbrefs
+    # Aliases
     #
+    alias h="history"
+    alias ls="exa --git --icons"
+    alias la="ls -la"
+    alias cat="bat"
 
+    #
+    # Abbrevs
     #
     # Fish
-    #
     abbr --add fial abbr --list
     abbr --add fis source ~/.config/fish/config.fish
     abbr --add fie hx ~/.config/fish/config.fish
     abbr --add fiv cat ~/.config/fish/config.fish
+    abbr --add .z --position anywhere '| fzf'
+    # Chezmoi
+    abbr --add cma chezmoi add
+    abbr --add cmc chezmoi cd
+    abbr --add cmd chezmoi diff
+    abbr --add cmm chezmoi managed
+    abbr --add cmr chezmoi re-add
+    abbr --add cms chezmoi status
+    # Git
+    abbr --add gis git status
+    abbr --add gid git diff    
 
     #
     # other utilities
     #
     abbr --add lg lazygit 
 
-
     #
-    # Aliases
+    # Starfish prompt
     #
-    alias h="history | fzf"
-    alias env="env | fzf"
-    alias ls="exa"
-    alias cat="bat"
-
+    starship init fish | source
 end
