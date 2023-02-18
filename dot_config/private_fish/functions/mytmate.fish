629f1fef8c25eefcf55c233f6f693aad7bb5df16
Christian Lemer <christian@lemer.be>
2023-02-19 00:58:14 +0100

    tmate: add a status window with bottom and htop.

--- dot_config/private_fish/functions/mytmate.fish
+++ dot_config/private_fish/functions/mytmate.fish
@@ -2,11 +2,10 @@ function mytmate
     set mytmatesocket (date +'/tmp/tmate-1000/%Y%m%d-%H%M%S-%N.sock')
 	tmate -S $mytmatesocket new-session \; \
 		send-keys 'q' \; \
-		rename-window -t 0 'config' \; \
-		send-keys -t 'config' 'cd ~/.config;hx .' C-m \; \
-		split-window -h \; \
-		send-keys -t 'config' 'cd ~/.config' C-m \; \
-		select-pane -t 0 \; \
+		rename-window -t 0 'status' \; \
+		send-keys -t 'status' 'btm' C-m \; \
+		split-window -v \; \
+		send-keys -t 'status' 'htop' C-m \; \
 		new-window -n 'hermes' \; \
 		send-keys -t 'hermes' 'cd ~/git/lemerco/hermes;hx .' C-m \; \
 		split-window -h \; \
@@ -14,8 +13,11 @@ function mytmate
 		new-window -n 'obsidian' \; \
 		send-keys -t 'obsidian' 'cd ~/vault; hx .' C-m \; \
 		select-window -t "hermes" \; \
+		new-window -n 'config' \; \
+		send-keys -t 'config' 'cd ~/.config;hx .' C-m \; \
+		split-window -h \; \
+		send-keys -t 'config' 'cd ~/.config' C-m \; \
 		select-pane -t 0 \; \
-		show-messages \; \
 
 	tmate -S $mytmatesocket display -p '#{tmate_ssh}' | xclip -selection clipboard
 
