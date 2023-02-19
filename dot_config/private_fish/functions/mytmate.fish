db9f7a5a5af522002a737bdf69a2213a3432d307
Christian Lemer <christian@lemer.be>
2023-02-19 10:40:34 +0100

    tmate: improve mytmate.

--- dot_config/private_fish/functions/mytmate.fish
+++ dot_config/private_fish/functions/mytmate.fish
@@ -10,13 +10,18 @@ function mytmate
 		send-keys -t 'hermes' 'cd ~/git/lemerco/hermes;hx .' C-m \; \
 		split-window -h \; \
 		send-keys -t 'hermes' 'cd ~/git/lemerco/hermes;cwn' C-m \; \
+		new-window -n 'hermes-git' \; \
+		send-keys -t 'hermes-git' 'cd ~/git/lemerco/hermes;lazygit' C-m \; \
 		new-window -n 'obsidian' \; \
-		send-keys -t 'obsidian' 'cd ~/vault; hx .' C-m \; \
-		select-window -t "hermes" \; \
+		send-keys -t 'obsidian' 'cd ~/.obsidian; hx .' C-m \; \
 		new-window -n 'config' \; \
 		send-keys -t 'config' 'cd ~/.config;hx .' C-m \; \
 		split-window -h \; \
 		send-keys -t 'config' 'cd ~/.config' C-m \; \
+		select-pane -t 0 \; \
+		new-window -n 'chezmoi' \; \
+		send-keys -t 'chezmoi' 'clear;chezmoi cd' C-m \; \
+		select-window -t "hermes" \; \
 		select-pane -t 0 \; \
 
 	tmate -S $mytmatesocket display -p '#{tmate_ssh}' | xclip -selection clipboard
